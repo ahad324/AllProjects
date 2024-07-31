14d9d111f1a28f79dd39e8413f19ec19a289b028
ahad <ahadg446@gmail.com>
2024-07-31 10:12:38 +0500

    Removed Unecessary code

--- src/components/UI/3d-card.tsx
+++ src/components/UI/3d-card.tsx
@@ -48,7 +48,6 @@ export const CardContainer = ({
     <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
       <div
         className={cn(
-          "py-20 flex items-center justify-center",
           containerClassName
         )}
         style={{
@@ -61,7 +60,6 @@ export const CardContainer = ({
           onMouseMove={handleMouseMove}
           onMouseLeave={handleMouseLeave}
           className={cn(
-            "flex items-center justify-center relative transition-all duration-200 ease-linear",
             className
           )}
           style={{
@@ -85,7 +83,6 @@ export const CardBody = ({
   return (
     <div
       className={cn(
-        "h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
         className
       )}
     >
@@ -136,7 +133,7 @@ export const CardItem = ({
   return (
     <Tag
       ref={ref}
-      className={cn("w-fit transition duration-200 ease-linear", className)}
+      className={cn(className)}
       {...rest}
     >
       {children}
